f04f93e1f149fd792ae030bc5688649e5910f16f
suryatharun4248 <suryatharuntejagajula@gmail.com>
2026-04-21 18:02:59 +0530

    Ready for Render deployment

--- weatherProject/settings.py
+++ weatherProject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +128,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 OPENWEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
